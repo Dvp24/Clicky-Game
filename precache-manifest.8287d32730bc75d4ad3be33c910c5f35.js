7fe3c183b6fc1851c21369bedfb273ba598f811d
Dhanashree <dvphalak@gmail.com>
2019-01-08 17:40:38 -0500

    Updates

---
+++ precache-manifest.8287d32730bc75d4ad3be33c910c5f35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "82fd637106d297512754",
+    "url": "/clicky-game/static/js/main.82fd6371.chunk.js"
+  },
+  {
+    "revision": "e9df87e63f8210124651",
+    "url": "/clicky-game/static/js/1.e9df87e6.chunk.js"
+  },
+  {
+    "revision": "82fd637106d297512754",
+    "url": "/clicky-game/static/css/main.f7a95613.chunk.css"
+  },
+  {
+    "revision": "26a34a11ccc6dae9c6c47c1dd50fdd01",
+    "url": "/clicky-game/index.html"
+  }
+];
